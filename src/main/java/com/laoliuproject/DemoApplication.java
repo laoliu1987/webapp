f17468c84da644928dc03925ef4a1f1c219e8540
老刘 <741038708@qq.com>
2020-12-15 00:03:23 +0800

    second commit

---
+++ src/main/java/com/laoliuproject/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.laoliuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+@RequestMapping("/test")
+    public @ResponseBody String test(){
+        return "test";
+}
+}
